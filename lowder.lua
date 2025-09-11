df8e703bff769ba4907e580e56d792d936cb8377
xxxrrxsds-art <xxxrrxsds@gmail.com>
2025-09-11 17:46:54 +0700

    Update lowder.lua

--- lowder.lua
+++ lowder.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local currentkey = "EXC-L3S-8D3-ARO-B93-8D-SEN-SE"
-local Window = Library.CreateLib("Execsense Loader", "DarkTheme")
+local Window = Library.CreateLib("execsense loader", "DarkTheme")
 
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Key System")
