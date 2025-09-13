9e109cd571536963e1c5aa1373af6ba0dd02460c
xxxrrxsds-art <xxxrrxsds@gmail.com>
2025-09-13 17:30:52 +0700

    Update lowder.lua

--- lowder.lua
+++ lowder.lua
@@ -1,51 +1,71 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local MacLib = loadstring(game:HttpGet("https://github.com/biggaboy212/Maclib/releases/latest/download/maclib.txt"))()
+local Window = MacLib:Window({
+	Title = "execsense loader",
+	Subtitle = "The Newest Loader",
+	Size = UDim2.fromOffset(650, 520),
+	DragStyle = 1,
+	DisabledWindowControls = {},
+	ShowUserInfo = true,
+	Keybind = Enum.KeyCode.RightControl,
+	AcrylicBlur = true,
+})
 local currentkey = "Abcdjfhhkghdskfkfkdkfkdfkghfhghfhhghhhfhhfhs"
-local Window = Library.CreateLib("execsense loader", "DarkTheme")
-
-local Tab = Window:NewTab("Main")
-local Section = Tab:NewSection("Key System")
-Section:NewTextBox("Key", "Enter the key", function(txt)
-	invitecode = txt
-end)
-
-local status = Section:NewLabel("Waiting...")
-Section:NewButton("Load", "Activate", function()
-    if invitecode == "useexecfree" then
-        status:UpdateLabel("Loading...")
-        if game.placeId == 4924922222 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/brbrhaven.lua"))()
-        elseif game.placeId == 1537690962 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/bssfree.lua"))()
-		elseif game.placeId == 6403373529 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/slapfree.lua"))()
-        elseif game.placeId == 11520107397 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/ksfree.lua"))()
-        elseif game.placeId == 124596094333302 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/slapfree.lua"))()
-        else
-            game.Players.LocalPlayer:Kick("Invalid Game.")
+local invitecode = nil
+local tabGroups = {
+	TabGroup1 = Window:TabGroup()
+}
+local tabs = {
+	Key = tabGroups.TabGroup1:Tab({ Name = "Key System"}),
+}
+local sections = {
+	MainSection1 = tabs.Key:Section({ Side = "Left" }),
+}
+sections.MainSection1:Input({
+	Name = "Input the Key",
+	Placeholder = "New Key",
+	AcceptedCharacters = "All",
+	Callback = function(input)
+		invitecode = input
+	end,
+}, "Input")
+sections.MainSection1:Button({
+	Name = "Load",
+	Callback = function()
+    	if invitecode == "useexecfree" then
+            if game.placeId == 4924922222 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/brbrhaven.lua"))()
+            elseif game.placeId == 1537690962 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/bssfree.lua"))()
+    		elseif game.placeId == 6403373529 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/slapfree.lua"))()
+            elseif game.placeId == 11520107397 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/ksfree.lua"))()
+            elseif game.placeId == 124596094333302 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/slapfree.lua"))()
+            else
+                game.Players.LocalPlayer:Kick("Not Supported Game.")
+            end
+        elseif invitecode == currentkey then
+            if game.placeId == 4924922222 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/brbrhaven.lua"))()
+            elseif game.placeId == 1537690962 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/BSSBeta.lua"))()
+            elseif game.placeId == 6403373529 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/slapea.lua"))()
+            elseif game.placeId == 11520107397 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/ksonly.lua"))()
+            elseif game.placeId == 124596094333302 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/slapea.lua"))()
+    		elseif game.placeId == 96342491571673 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/stealabrain.lua"))()
+    		elseif game.placeId == 109983668079237 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/stealabrain.lua"))()
+            else
+                game.Players.LocalPlayer:Kick("Not Supported Game.")    
+            end
         end
-    elseif invitecode == currentkey then
-        status:UpdateLabel("Loading...")
-        if game.placeId == 4924922222 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/brbrhaven.lua"))()
-        elseif game.placeId == 1537690962 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/BSSBeta.lua"))()
-        elseif game.placeId == 6403373529 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/slapea.lua"))()
-        elseif game.placeId == 11520107397 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/ksonly.lua"))()
-        elseif game.placeId == 124596094333302 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/slapea.lua"))()
-		elseif game.placeId == 96342491571673 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/stealabrain.lua"))()
-		elseif game.placeId == 109983668079237 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/xxxrrxsds-art/losehubs/refs/heads/main/stealabrain.lua"))()
-        end
-         
-    else
-        status:UpdateLabel("Invalid!")
-    end
-end)
-local freekey = Section:NewLabel("You can get a free key in our Discord")
-local freekey1 = Section:NewLabel("discord.gg/XAqn48p6Jm")
+	end,
+})
+sections.MainSection1:Label({
+	Text = "You can get free key in our Discord - discord.gg/XAqn48p6Jm"
+})
