c9ca4d45c73863a70e278fc3bd94e3f7d57652a1
xxxrrxsds-art <xxxrrxsds@gmail.com>
2025-09-13 16:09:13 +0700

    Update slapfree.lua

--- slapfree.lua
+++ slapfree.lua
@@ -265,15 +265,9 @@ sections.MainSection9:Button({
 		end
 	end,
 })
-if tostring(getgenv().invitecode) ~= "nil" then
-    sections.MainSection8:Label({
-        Text = "Build: Free"
-    })
-else 
-    sections.MainSection8:Label({
-        Text = "Build: Source/Dev"
-    })
-end
+sections.MainSection8:Label({
+    Text = "Version: Free"
+})
 sections.MainSection8:Label({
 	Text = "Our Discord: discord.gg/XAqn48p6Jm"
 })
