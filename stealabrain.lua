a1d494c35cd531ef43dc4e6501eef8786f256726
xxxrrxsds-art <xxxrrxsds@gmail.com>
2025-09-13 15:29:26 +0700

    Update stealabrain.lua

--- stealabrain.lua
+++ stealabrain.lua
@@ -1,7 +1,7 @@
 local MacLib = loadstring(game:HttpGet("https://github.com/biggaboy212/Maclib/releases/latest/download/maclib.txt"))()
 local Window = MacLib:Window({
 	Title = "execsense",
-	Subtitle = "Steal a Brainrot: Early Beta V1",
+	Subtitle = "Steal a Brainrot: Early Beta V1.1",
 	Size = UDim2.fromOffset(868, 650),
 	DragStyle = 1,
 	DisabledWindowControls = {},
@@ -15,23 +15,28 @@ local tabGroups = {
 local tabs = {
 	AC = tabGroups.TabGroup1:Tab({ Name = "Anti-Cheat Bypass"}),
 	Oth = tabGroups.TabGroup1:Tab({ Name = "Other"}),
+	bas = tabGroups.TabGroup1:Tab({ Name = "Base"}),
 }
 local sections = {
 	MainSection1 = tabs.AC:Section({ Side = "Left" }),
 	MainSection2 = tabs.Oth:Section({ Side = "Left" }),
+	MainSection3 = tabs.bas:Section({ Side = "Left" }),
 }
 
 sections.MainSection1:Header({
 	Name = "Main"
 })
+sections.MainSection3:Header({
+	Name = "Money"
+})
+local plr = game.Players.LocalPlayer
+local antitp = true
+local prevatp = nil
+local newpos = nil
 sections.MainSection1:Toggle({
 	Name = "Teleport Bypass V1 [DETECTED]",
 	Default = false,
 	Callback = function(value)
-		local plr = game.Players.LocalPlayer
-        local antitp = true
-        local prevatp = nil
-        local newpos = nil
         getgenv().bypas = value
         atp = true
         if getgenv().bypas then
@@ -59,6 +64,45 @@ sections.MainSection2:Button({
             elseif i.Name == "LaserHitbox" then
                 i:Destroy()
             end
+        end
+	end,
+})
+sections.MainSection1:Toggle({
+	Name = "Magic Body [UNDETECTED]",
+	Default = false,
+	Callback = function(value)
+	    getgenv().magic = value
+        if getgenv().magic then    
+            MagicPrev = plr.Character.PrimaryPart.CFrame
+			plr.Character.PrimaryPart.CFrame = CFrame.new(0, 1000, 0)
+            task.wait()
+            plr.Character.PrimaryPart.Anchored = true
+            task.wait()
+            plr.Character.PrimaryPart.CFrame = MagicPrev
+            local mouse = game.Players.LocalPlayer:GetMouse()
+            local input = game:GetService("UserInputService")
+            mouse.Button1Down:Connect(function()
+                if getgenv().magic then    
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(mouse.Hit.Position)
+                    plr.Character.PrimaryPart.Anchored = true
+                end
+            end)
+            input.InputBegan:Connect(function(key)
+                if key.KeyCode == Enum.KeyCode.E and getgenv().magic then
+                    plr.Character.PrimaryPart.Anchored = false
+                end
+            end)
+        else
+            plr.Character.PrimaryPart.Anchored = false
+        end
+	end,
+}, "Toggle")
+sections.MainSection3:Button({
+	Name = "Instant Collect $ (requires to be close to Brainrot)",
+	Callback = function()
+		for i = 1, 50 do
+            task.wait(0.15)
+            game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Net"):WaitForChild("RE/PlotService/ClaimCoins"):FireServer(i)
         end
 	end,
 })
