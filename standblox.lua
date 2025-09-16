376af978324edd6044aed1920686622edbe82485
xxxrrxsds-art <xxxrrxsds@gmail.com>
2025-09-16 08:31:52 +0700

    Update standblox.lua

--- standblox.lua
+++ standblox.lua
@@ -1,17 +1,34 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("TITLE", "DarkTheme")
+local Window = Library.CreateLib("Standblox Remade", "DarkTheme")
 local Tab = Window:NewTab("Ragebot")
-local Section = Tab:NewSection("Ragebot V1")
+local Section = Tab:NewSection("Ragebot V2")
+local Section2 = Tab:NewSection("Local Player")
+local infinite = false
+local god = false
+local ns = false
+local spr = nil
+Section:NewToggle("Endless Ammo", "working with Auto Fire only", function(state)
+    infinite = state
+end)
+Section2:NewToggle("God Mode", "working with Auto Fire only", function(state)
+    god = true
+end)
+local needed = nil
 Section:NewToggle("Auto Fire", "Through walls - Always On", function(state)
-    _G.afire = state
+    getgenv().afire = state
+    
+    if getgenv().afire then
+        needed = game.Players.LocalPlayer.team.Value  
+    end
+    
     while getgenv().afire and task.wait() do
         for _, i in pairs(game.Players:GetPlayers()) do  
             if i.Character and i.Name ~= game.Players.LocalPlayer.Name then
-                if i.Character and i.Character:FindFirstChild("Head") then
+                if i.Character and i.Character:FindFirstChild("Head") and i.team.Value ~= needed then
                     local args = {
                     	{
                     		muzzle_position = i.Character.Head.Position + Vector3.new(0.0001, 0.00001, 0.00001),
-                    		keep_ammo = false,
+                    		keep_ammo = infinite,
                     		is_stitched = false,
                     		weapon_name = game.Players.LocalPlayer.secondary.Value,
                     		cframe = game.Players.LocalPlayer.Character.LowerTorso.CFrame,
@@ -19,12 +36,12 @@ Section:NewToggle("Auto Fire", "Through walls - Always On", function(state)
                     			crouching = false,
                     			aimming = false,
                     			shifting = false,
-                    			strength = streng,
+                    			strength = 10,
                     			falling = false,
                     			running = false,
                     			jumping = false,
                     			flashed = false,
-                    			god_mode = false
+                    			god_mode = god
                     		},
                     		spread_cframe = CFrame.new(0, 0, 0, 0, -0, 0, 0, 0, -0, -0, 0, 0)
                     	},
